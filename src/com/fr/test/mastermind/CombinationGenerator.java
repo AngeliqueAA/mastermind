4117e49d09758365314e80d63f7973a21b91f2f9
angelique.a <angelique.assouvie@gmail.com>
2021-04-15 13:08:32 +0200

    tdd for translating integer to color and generate array of random color

--- src/com/fr/test/mastermind/CombinationGenerator.java
+++ src/com/fr/test/mastermind/CombinationGenerator.java
@@ -1,12 +1,28 @@
 package com.fr.test.mastermind;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.fr.shared.mastermind.Color;
 
 public class CombinationGenerator {
 
 	public Color translateToColor(Integer translateToColor) {
-		// TODO Auto-generated method stub
-		return Color.RED;
+		Color translatedColor = Color.values()[translateToColor];
+		return translatedColor;
+	}
+
+	public List<Color> generateRandomArrayOfColor() {
+
+		List<Color> arrayOfRandomColor = new ArrayList<Color>();
+		Random random = new Random();
+		int maxColorValue = 4;
+
+		Color generatedColor = translateToColor(random.nextInt(maxColorValue));
+		arrayOfRandomColor.add(generatedColor);
+
+		return arrayOfRandomColor;
 	}
 
 }
--- src/com/fr/test/mastermind/CombinationGeneratorTest.java
+++ src/com/fr/test/mastermind/CombinationGeneratorTest.java
@@ -1,6 +1,9 @@
 package com.fr.test.mastermind;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +23,48 @@ class CombinationGeneratorTest {
 
 		// then integer have to match with a color
 		assertEquals(Color.RED, generatedColor);
+	}
+
+	@Test
+	void shouldReturnGreenOnIntegerOne() {
+
+		// given an integer
+		CombinationGenerator combinationGenerator = new CombinationGenerator();
+		Integer translateToColor = 1;
+
+		// when we check the integer
+		Color generatedColor = combinationGenerator.translateToColor(translateToColor);
+
+		// then integer have to match with a color
+		assertEquals(Color.GREEN, generatedColor);
+	}
+
+	@Test
+	void shouldReturnYellowOnIntegerTwo() {
+
+		// given an integer
+		CombinationGenerator combinationGenerator = new CombinationGenerator();
+		Integer translateToColor = 2;
+
+		// when we check the integer
+		Color generatedColor = combinationGenerator.translateToColor(translateToColor);
+
+		// then integer have to match with a color
+		assertEquals(Color.YELLOW, generatedColor);
+	}
+
+	@Test
+	void shouldReturnAnArrayOfThreeRandomColor() {
+
+		// given an array of color
+		CombinationGenerator combinationGenerator = new CombinationGenerator();
+		List<Color> firstColorsToGuess = combinationGenerator.generateRandomArrayOfColor();
+
+		// when we generate another array of color
+		List<Color> secondColorsToGuess = combinationGenerator.generateRandomArrayOfColor();
+
+		// then the two arrays have to be different
+		assertNotEquals(firstColorsToGuess, secondColorsToGuess);
 	}
 
 }
